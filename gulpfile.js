a1757dc07537aaa185cde70ec0d3004d06c801c5
xdr3am <xdreamroyale@yandex.ru>
2020-09-13 15:33:40 +0300

    Внес исправления после 1 попытки защиты проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const csso = require("gulp-csso");
-const rename = require("gulp-rename");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
-const svgstore = require("gulp-svgstore");
-const del = require("del");
-const htmlmin = require("gulp-htmlmin");
-const uglify = require("gulp-uglify");
-const pipeline = require("readable-stream").pipeline;
-const sync = require("browser-sync").create();
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+const pipeline = require('readable-stream').pipeline;
+const sync = require('browser-sync').create();
 
 // Clean
 
 const clean = () => {
-  return del("build");
+  return del('build');
 };
 
 exports.clean = clean;
@@ -29,16 +29,16 @@ const copy = () => {
   return gulp
     .src(
       [
-        "source/fonts/**/*.{woff,woff2}",
-        "source/img/**",
-        "source/js/**",
-        "source/*.html",
+        'source/fonts/**/*.{woff,woff2}',
+        'source/img/**',
+        'source/js/**',
+        'source/*.html',
       ],
       {
-        base: "source",
+        base: 'source',
       }
     )
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest('build'));
 };
 
 exports.copy = copy;
@@ -47,16 +47,16 @@ exports.copy = copy;
 
 const styles = () => {
   return gulp
-    .src("source/sass/style.scss")
+    .src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest('build/css'))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css'))
     .pipe(sync.stream());
 };
 
@@ -66,7 +66,7 @@ exports.styles = styles;
 
 const images = () => {
   return gulp
-    .src("source/img/**/*.{jpg,png,svg}")
+    .src('source/img/**/*.{jpg,png,svg}')
     .pipe(
       imagemin([
         imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -82,9 +82,9 @@ exports.images = images;
 
 const createWebp = () => {
   return gulp
-    .src("source/img/**/*.{jpg,png}")
+    .src('source/img/**/*.{jpg,png}')
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest('source/img'));
 };
 
 exports.createWebp = createWebp;
@@ -93,10 +93,10 @@ exports.createWebp = createWebp;
 
 const sprite = () => {
   return gulp
-    .src("source/img/**/icon-*.svg")
+    .src('source/img/**/icon-*.svg')
     .pipe(svgstore())
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 };
 
 exports.sprite = sprite;
@@ -105,9 +105,10 @@ exports.sprite = sprite;
 
 const html = () => {
   return gulp
-    .src("source/*.html")
+    .src('source/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest('build'))
+    .pipe(sync.stream())
 };
 
 exports.html = html;
@@ -115,7 +116,11 @@ exports.html = html;
 // Js
 
 const jsmin = () => {
-  return gulp.src("source/js/*.js").pipe(uglify()).pipe(gulp.dest("build/js"));
+  return gulp.src
+    ('source/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(sync.stream())
 };
 
 exports.jsmin = jsmin;
@@ -125,7 +130,7 @@ exports.jsmin = jsmin;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: "build",
+      baseDir: 'build',
     },
     cors: true,
     notify: false,
@@ -139,8 +144,9 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/*.html', gulp.series(html));
+  gulp.watch('source/js', gulp.series(jsmin))
 };
 
 // build
--- source/js/modal-cart.js
+++ source/js/modal-cart.js
@@ -1,24 +1,19 @@
-var toOrder = document.querySelectorAll(".to-order");
-var cart = document.querySelector(".modal-cart");
-var addToCart = document.querySelector(".modal-cart__button");
+var toOrder = document.querySelectorAll('.to-order');
+var cart = document.querySelector('.modal-cart');
+var addToCart = document.querySelector('.modal-cart__button');
 
 for (var i = 0; i < toOrder.length; i++) {
-  toOrder[i].addEventListener("click", function (evt) {
+  toOrder[i].addEventListener('click', function (evt) {
     evt.preventDefault();
-    cart.classList.add("modal-cart--show");
+    cart.classList.add('modal-cart--show');
   })
 };
 
-addToCart.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cart.classList.remove("modal-cart--show");
-});
-
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (cart.classList.contains("modal-cart--show")) {
+    if (cart.classList.contains('modal-cart--show')) {
       evt.preventDefault();
-      cart.classList.remove("modal-cart--show");
+      cart.classList.remove('modal-cart--show');
     }
   }
 });
